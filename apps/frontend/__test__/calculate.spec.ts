4c0e19bf957feaf8c0df25dc0ef699b0f721ac59
buraito-kun <brightrock19220@outlook.co.th>
2025-05-15 16:25:46 +0700

    test(frontend): fix typo

--- apps/frontend/__test__/calculate.spec.ts
+++ apps/frontend/__test__/calculate.spec.ts
@@ -56,17 +56,17 @@ describe("test lib/calculate.ts", () => {
     expect(await fetchCalculation(99, -11, "/")).toBe(-9);
   });
 
-  it("exponent 2 / 3 should equal 8", async () => {
+  it("exponent 2 ^ 3 should equal 8", async () => {
     (axios.post as jest.Mock).mockImplementation(() =>
       Promise.resolve({ status: 201, data: 8 })
     );
-    expect(await fetchCalculation(2, 3, "/")).toBe(8);
+    expect(await fetchCalculation(2, 3, "^")).toBe(8);
   });
-  it("exponent 2 / -2 should equal 0.25", async () => {
+  it("exponent 2 ^ -2 should equal 0.25", async () => {
     (axios.post as jest.Mock).mockImplementation(() =>
       Promise.resolve({ status: 201, data: 0.25 })
     );
-    expect(await fetchCalculation(2, -2, "/")).toBe(0.25);
+    expect(await fetchCalculation(2, -2, "^")).toBe(0.25);
   });
 
   it("other op 2 ** 3 should equal NaN", async () => {
